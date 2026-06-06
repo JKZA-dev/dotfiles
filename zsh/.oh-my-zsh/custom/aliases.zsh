945fd2ba6fbd009c08be0dc052916603be282230
JKZA <Josua.Zabel@tu-dortmund.de>
2026-06-06 13:33:18 +0200

    add new ohmyzsh folder

---
+++ zsh/.oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,22 @@
+# Set personal aliases, overriding those provided by Oh My Zsh libs,
+# plugins, and themes. Aliases can be placed here, though Oh My Zsh
+# users are encouraged to define aliases within a top-level file in
+# the $ZSH_CUSTOM folder, with .zsh extension. Examples:
+# - $ZSH_CUSTOM/aliases.zsh
+# - $ZSH_CUSTOM/macos.zsh
+# For a full list of active aliases, run `alias`.
+#
+# Example aliases
+# alias zshconfig="mate ~/.zshrc"
+# alias ohmyzsh="mate ~/.oh-my-zsh"
+
+alias ll="ls -l"
+alias la="ls -a"
+alias v="nvim"
+alias sv="sudoedit"
+#NeoVIM zu sudo editor machen
+export SUDO_EDITOR="/usr/bin/zsh"
+#
+alias get="sudo dnf install"
+alias yeet="rm"
+alias ff="exit"
---
+++ zsh/.oh-my-zsh/custom/startupcode.zsh
@@ -0,0 +1,10 @@
+# Direkte Ausgabe beim starten erlauben:
+# typeset -g POWERLEVEL9K_INSTANT_PROMPT=quiet
+
+#Custom Code on Startup von mir selbst:
+zsh --version
+fastfetch --pipe false
+cd
+print -l "Einen guten Tag mein Herr, sie befinden sich auf ihrem" 
+cat ~/.oh-my-zsh/custom/Device.txt
+print -l "" "Ich wünsche einen Produktiven Tag!"
